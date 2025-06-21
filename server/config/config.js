b2928d820815bd5c4282c4573610c4191ac52463
hhoang777 <hhoang1072003@gmail.com>
2025-06-21 13:42:26 +0700

    config db production

--- server/config/config.js
+++ server/config/config.js
@@ -6,13 +6,20 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    dialect: process.env.DB_DIALECT || "postgres",
+    timezone: "+07:00",
+    logging: false,
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    timezone: "+07:00",
+    logging: false,
   },
 };
